4cf62cb4a5327f4fb2734f3a85cd8c0b248e8391
Mario Butler <mbutlertechtraining@DESKTOP-EHPAM6P>
2022-03-08 20:59:16 -0500

    Modified index

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# webapichallenge
+# quiz_taker  
+ 
+## Table Of Contents:
+- [Description](#Description)
+- [Deployment](#Deployment)
+- [Installation](#Installation)
+- [License](#License)
+
+## Description
+As a user, I want to be able to take a quiz where I am prompted with questions and answer options.  If I select the correct answer, Iam provided feedback.  If I select the incorrect answer, I am provided feedback and time of availability will be reduced.  At the end of the quiz, I will be prompted to submit my initials where they are saved to local storage.   
+
+## Deployment:
+GitHub Repo: https://github.com/mbutlertechtraining/webapichallenge
+
+GitHub Pages: https://mbutlertechtraining.github.io/webapichallenge/
+
+![picture](image.png)
+
+## Installation:
+1. No installation if needed
+2. You may view code through access of GitHub, link provided above.
+3. You may view site through GitHub Pages, link provided above.
+
+## License
+
+MIT License
+Copyright (c)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+
---
+++ assest/js/quizz.js
@@ -0,0 +1,181 @@
+//Creating our array of questions with answer choices and correct answers
+var questions = [
+    {
+        title:"What is the capital of the US state, Alabama?",
+        choices:[ "Montgomery", "Birmingham", "Texas", "Appleton"],
+        answer:"Montgomery"
+    },
+    {
+      title:"What is the capital of the US state, Michigan?",
+      choices:[ "Detroit", "Grand Rapids", "Lansing", "East Lansing"],
+      answer:"Lansing"
+    },
+
+    {
+      title:"Which shoe company sells Air Jordan shoe brand?",
+      choices:[ "Adidas", "Puma", "New Balance", "Nike"],
+      answer:"Nike"
+    },
+
+    {
+      title:"What will an Apple a day keep away from you?",
+      choices:[ "Sun light ", "The Doctor", "Air", "Your Mother-In-law"],
+      answer:"The Doctor"
+    }
+
+
+    
+];
+
+
+
+//reaching into the DOM based on ID
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+//Reaching into the DOM to display.  In the timerID, we are setting the time in Inter
+function startQuiz() {
+ 
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  questionsEl.removeAttribute("class");
+
+  timerId = setInterval(clockTick, 1000);
+
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+//reaching into the DOM by utilizing the ID and setting the attributes to the questions
+function getQuestion() {
+
+  var currentQuestion = questions[currentQuestionIndex];
+
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  choicesEl.innerHTML = "";
+
+  currentQuestion.choices.forEach(function(choice, i) {
+  
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    choiceNode.onclick = questionClick;
+
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+//checking if the response if correct of not.  Time is reduced by 15 seconds also providing feedback
+function questionClick() {
+
+  if (this.value !== questions[currentQuestionIndex].answer) {
+  
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    timerEl.textContent = time;
+    feedbackEl.textContent = "Wrong!";
+  } else {
+  
+    feedbackEl.textContent = "Correct!";
+  }
+
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+ 
+  //calculating the number of correct and incorrect questions
+  currentQuestionIndex++;
+
+  //represent the quiz ending 
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+//Sending to the end screen and final scores and time
+function quizEnd() {
+  
+  clearInterval(timerId);
+
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  questionsEl.setAttribute("class", "hide");
+}
+
+//Going through time and subtracting as needed 
+function clockTick() {
+
+  time--;
+  timerEl.textContent = time;
+
+  //if statement to say if zero is reach to end the quiz
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function saveHighscore() {
+
+  //taking the initials and saving them to local scores 
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+ 
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+      //The way new scores are plugged in
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    window.location.href = "highscores.html";
+  }
+}
+
+//Once the initials are enter, the information is saved 
+function checkForEnter(event) {
+
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+//Calling the functions when there is a click on event.key 
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
